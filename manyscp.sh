40413dc4a483d4013ac54b8c785c57228c1eded0
Dom <devdom4g@gmail.com>
2017-03-19 19:35:22 -0700

    Update manyscp.sh

--- manyscp.sh
+++ manyscp.sh
@@ -7,19 +7,18 @@ export pass
  
 echo "Copying the file"
 SCRIPT='
-			package require Tcl 8.4
-			package require Expect 5.40
-			set HOSTS "host1 host2 host3 host4"
-			foreach host $HOSTS {
-									spawn sh -c "scp -C -o StrictHostKeyChecking=no ./filename.txt root@$host:/tmp";
-									sleep 3;
-									expect "*?assword:*"
-									send -- "$env(pass)\r"
-									expect 100%
-									send -- "exit\r"
-									expect eof
+	package require Tcl 8.4
+	package require Expect 5.40
+	set HOSTS "host1 host2 host3 host4"
+	foreach host $HOSTS {
+				spawn sh -c "scp -C -o StrictHostKeyChecking=no ./filename.txt root@$host:/tmp";
+				sleep 3;
+				expect "*?assword:*"
+				send -- "$env(pass)\r"
+				expect 100%
+				send -- "exit\r"
+				expect eof
 			}
 '
 /usr/bin/expect << HERE
 $SCRIPT
- 
